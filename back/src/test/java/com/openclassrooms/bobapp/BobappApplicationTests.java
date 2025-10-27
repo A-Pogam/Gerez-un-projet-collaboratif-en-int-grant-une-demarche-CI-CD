f4b81b9812de188920901b2336b3c155de0bffbb
A-Pogam <alicepogam59@hotmail.com>
2025-10-27 19:21:52 +0100

    test

--- back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
+++ back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
@@ -2,6 +2,8 @@ package com.openclassrooms.bobapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class BobappApplicationTests {
